adcd67c2c826cd9a885953cf3e24207ff6b3aeb8
dehrexd <dehrexd@gmail.com>
2025-07-08 18:44:42 +0800

    basic handling of input

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include "include/colors.h"
 
-int main(int argc, char **argv)
+const int run(int day)
 {
-    printf("Hello, from advent_of_c!\n");
+    switch (day)
+    {
+    case 1:
+        printf("Hello World");
+        break;
+
+    default:
+        printf("Not yet implemented");
+        return 1;
+    }
+    return 0;
+}
+
+const int main(int argc, char *argv[])
+{
+    if (argc >= 3)
+    {
+        
+    }
+    else
+    {
+        printf("%s\nAdvent of C\n\nUsage:\n  aoc [year] [day(s)]\n\n%s", BHRED, COLOR_RESET);
+        return 1;
+    };
+    return 0;
 }
