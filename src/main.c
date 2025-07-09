ed2562f588a5a032ca306ac7c91d10959e928765
dehrexd <dehrexd@gmail.com>
2025-07-09 08:20:28 +0800

    boilerplate and refactoring

--- src/main.c
+++ src/main.c
@@ -4,29 +4,58 @@
 extern int printf(const char *__restrict __format, ...);
 extern int strcmp(const char *__s1, const char *__s2) __THROW __attribute_pure__
     __nonnull((1, 2));
+extern int atoi(const char *__nptr) __THROW __attribute_pure__
+    __nonnull((1)) __wur;
 
 #define BHRED "\033[1;91m"
 #define COLOR_RESET "\033[0m"
+int DAY_1(void) { return 0; }
+int DAY_2(void) { return 0; }
+int DAY_3(void) { return 0; }
+int DAY_4(void) { return 0; }
+int DAY_5(void) { return 0; }
+int DAY_6(void) { return 0; }
+int DAY_7(void) { return 0; }
+int DAY_8(void) { return 0; }
+int DAY_9(void) { return 0; }
+int DAY_10(void) { return 0; }
+int DAY_11(void) { return 0; }
+int DAY_12(void) { return 0; }
+int DAY_13(void) { return 0; }
+int DAY_14(void) { return 0; }
+int DAY_15(void) { return 0; }
+int DAY_16(void) { return 0; }
+int DAY_17(void) { return 0; }
+int DAY_18(void) { return 0; }
+int DAY_19(void) { return 0; }
+int DAY_20(void) { return 0; }
+int DAY_21(void) { return 0; }
+int DAY_22(void) { return 0; }
+int DAY_23(void) { return 0; }
+int DAY_24(void) { return 0; }
+int DAY_25(void) { return 0; }
 
-const int run(const char *year_day) {
-  if (strcmp(year_day, "2024-1") == 0) {
-    printf("Hello World!\n");
-  }
+typedef int (*FunctionPointer)(void);
+const FunctionPointer DAYS[25] = {&DAY_1,  &DAY_2,  &DAY_3,  &DAY_4,  &DAY_5,
+                                  &DAY_6,  &DAY_7,  &DAY_8,  &DAY_9,  &DAY_10,
+                                  &DAY_11, &DAY_12, &DAY_13, &DAY_14, &DAY_15,
+                                  &DAY_16, &DAY_17, &DAY_18, &DAY_19, &DAY_20,
+                                  &DAY_21, &DAY_22, &DAY_23, &DAY_24, &DAY_25};
 
-  printf("\nUnhandled Year/Day: %s\n", year_day);
+int run(int day) {
+  if (DAYS[day])
+    return DAYS[day]();
+
+  printf("\nUnhandled Day: %i\n", day);
   return 1;
 }
 
-const int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   if (argc == 2) {
-    const char *year_day = argv[1];
+    run(atoi(argv[1]));
+    return 0;
+  }
 
-    printf("Starting day:  %s\n", year_day);
-    run(year_day);
-  } else {
-    printf("%s\nAdvent of C\n\nUsage:\n  aoc [year]-[day]\n\n%s", BHRED,
-           COLOR_RESET);
-    return 1;
-  };
-  return 0;
+  printf("%s\nAdvent of C\n\nUsage:\n  aoc [day]\n\n%s", BHRED, COLOR_RESET);
+  return 1;
 }
